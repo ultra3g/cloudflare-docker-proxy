99a3a560e2bdba30e404efb19f4c3e8c46164299
ultra3g <33754813+ultra3g@users.noreply.github.com>
2024-06-26 13:57:55 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.ultra6g.com": dockerHub,
+  "quay.ultra6g.com": "https://quay.io",
+  "gcr.ultra6g.com": "https://gcr.io",
+  "k8s-gcr.ultra6g.com": "https://k8s.gcr.io",
+  "k8s.ultra6g.com": "https://registry.k8s.io",
+  "ghcr.ultra6g.com": "https://ghcr.io",
+  "cloudsmith.ultra6g.com": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ultra6g.com": dockerHub,
 };
 
 function routeByHosts(host) {
